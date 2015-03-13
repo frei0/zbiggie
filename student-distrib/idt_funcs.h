523f505c79710cce070a15967716d8d2b6f09e67
btmoore95 <BenooreM@wirelessprvnat-172-17-4-166.near.illinois.edu>
2015-03-13 00:15:29 -0500

    Added more Interrupt Functions
    Added functions 8 - 19

--- student-distrib/idt_funcs.h
+++ student-distrib/idt_funcs.h
@@ -9,6 +9,23 @@ extern void breakpoint();
 extern void overflow();
 extern void BOUND_range_exceeded();
 extern void invalid_opcode();
+extern void device_not_available();
+extern void double_fault();
+extern void coprocessor_segment_overrun();
+extern void invalid_TSS();
+extern void segment_not_present();
+extern void stack_segment_fault();
+extern void general_protection();
+extern void page_fault();
+//15 Intel Reserved
+extern void floating_point_error();
+extern void alignment_check();
+extern void machine_check();
+extern void SIMD_floating_point_exception();
 
+//20-31 Intel Reserved
+
+//32-255
+//User Defined: Call common interrupt
 
 #endif
