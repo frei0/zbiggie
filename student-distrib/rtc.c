9a6945bbdc67823be6a3e4fad8bf122aae1af9a2
Bill Pendavinji <bpendavinji@localhost.localdomain>
2015-03-15 00:01:07 -0500

    dont know how to branch, doesn't break anything

---
+++ student-distrib/rtc.c
@@ -0,0 +1,27 @@
+/* i8259.c - Functions to interact with the 8259 interrupt controller
+ * vim:ts=4 noexpandtab
+ */
+
+#include "i8259.h"
+#include "rtc.h"
+#include "x86_desc.h"
+#include "lib.h"
+
+/* Initialize the rtc
+ *
+ * interrupts must be disabled
+ */
+void
+rtc_init(void)
+{
+    OUTB(RTC_INDEX_PORT,0x8A); //select port A, and disable NMI at same time
+    OUTB(RTC_RW_PORT,0x20);
+
+// enable irq 8
+    OUTB(RTC_INDEX_PORT, 0x8B); //select port B, keep NMI disabled
+    char current_b = INB(RTC_RW_PORT);
+    OUTB(RTC_INDEX_PORT, 0x8B); //select port B, keep NMI disabled
+    OUTB(RTC_RW_PORT, current_b | 0x40); //enable IR
+    OUTB(RTC_INDEX_PORT, 0x00); //enable NMI again
+    enable_irq(8);
+}
