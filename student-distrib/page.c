5c15ec055f660d40ac6500409e0958fc6d44bb58
Rachel Frei <1476684+frei0@users.noreply.github.com>
2015-03-17 02:22:40 -0500

    magic, less of it

--- student-distrib/page.c
+++ student-distrib/page.c
@@ -4,10 +4,12 @@
 #define PDE_USER           0x4 
 #define PDE_SIZE           0x80
 #define PDE_ADDRESS_MASK 0xFFFFF000
+#define PD_NUM_ENTRIES   1024
 
-#define PDT_PRESENT        0x1
-#define PDT_WRITE          0x2
-#define PDT_USER           0x4
+#define PTE_PRESENT        0x1
+#define PTE_WRITE          0x2
+#define PTE_USER           0x4
+#define PT_NUM_ENTRIES   1024
 
 #define OFFSET_4M      0x400000
 #define OFFSET_4K      0x1000
@@ -16,16 +18,16 @@
 #define PAGING_PSE    0x10
 #define PAGING_ENABLE 0x80000000
 
-unsigned page_directory[1024] __attribute__((aligned(OFFSET_4K))); //page directory 
-unsigned low_memory_table[1024] __attribute__((aligned(OFFSET_4K))); //first 4M, for mapping video
+unsigned page_directory[PD_NUM_ENTRIES] __attribute__((aligned(OFFSET_4K))); //page directory 
+unsigned low_memory_table[PT_NUM_ENTRIES] __attribute__((aligned(OFFSET_4K))); //first 4M, for mapping video
 
 void init_paging(void){
 	int i;
-	for (i = 0; i < 1024; ++i)
+	for (i = 0; i < PD_NUM_ENTRIES; ++i)
 		page_directory[i]=0x0; //fill page directory with non present entries
 
 
-    for (i = 0; i < 1024; i++) 
+    for (i = 0; i < PT_NUM_ENTRIES; i++) 
 		low_memory_table[i] = (i * OFFSET_4K); 
 
     page_directory[0] = (((unsigned int)low_memory_table) & PDE_ADDRESS_MASK)| PDE_PRESENT;
