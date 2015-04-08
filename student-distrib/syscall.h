5b46953844571587a058e74319d6aec49b54aa66
Bill <bpenda94@gmail.com>
2015-04-07 22:52:47 -0500

    syscall linkage does something now

--- student-distrib/syscall.h
+++ student-distrib/syscall.h
@@ -1,6 +1,10 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 #ifndef ASM
+#include "syscall_funcs.h"
+#include "lib.h"
+
+extern void syscall();
 
 #endif
 #endif
--- student-distrib/syscall_funcs.c
+++ student-distrib/syscall_funcs.c
@@ -2,40 +2,54 @@
 
 int halt_call(){
 
+    asm volatile(
+            "movl $1, %eax;"
+            "int $0x80;"
+            );
+    return 0;
 }
 
 int execute_call(){
 
+    return 0;
 }
 
 int read_call(){
 
+    return 0;
 }
 
 int write_call(){
 
+    return 0;
 }
 
 int open_call(){
 
+    return 0;
 }
 
 int close_call(){
 
+    return 0;
 }
 
 int getargs_call(){
 
+    return 0;
 }
 
 int vidmap_call(){
 
+    return 0;
 }
 
 int set_handler_call(){
 
+    return 0;
 }
 
 int sigreturn_call(){
 
+    return 0;
 }
