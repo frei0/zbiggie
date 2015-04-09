9b88a281e0a1505a882e352e9af79b703af940a0
Bill <bpenda94@gmail.com>
2015-04-08 23:40:30 -0500

    made an array of page directories

--- student-distrib/page.c
+++ student-distrib/page.c
@@ -5,6 +5,8 @@
 #define PDE_SIZE           0x80
 #define PDE_ADDRESS_MASK 0xFFFFF000
 #define PD_NUM_ENTRIES   1024
+#define NUM_PDS 7
+#define KERNEL_PD 0
 
 #define PTE_PRESENT        0x1
 #define PTE_WRITE          0x2
@@ -18,21 +20,21 @@
 #define PAGING_PSE    0x10
 #define PAGING_ENABLE 0x80000000
 
-unsigned page_directory[PD_NUM_ENTRIES] __attribute__((aligned(OFFSET_4K))); //page directory 
+unsigned page_directories[NUM_PDS][PD_NUM_ENTRIES] __attribute__((aligned(OFFSET_4K))); //page directory 
 unsigned low_memory_table[PT_NUM_ENTRIES] __attribute__((aligned(OFFSET_4K))); //first 4M, for mapping video
 
 void init_paging(void){
 	int i;
 	for (i = 0; i < PD_NUM_ENTRIES; ++i)
-		page_directory[i]=0x0; //fill page directory with non present entries
+		page_directories[KERNEL_PD][i]=0x0; //fill page directory with non present entries
 
 
     for (i = 0; i < PT_NUM_ENTRIES; i++) 
 		low_memory_table[i] = (i * OFFSET_4K); 
 
-    page_directory[0] = (((unsigned int)low_memory_table) & PDE_ADDRESS_MASK)| PDE_PRESENT;
+    page_directories[KERNEL_PD][0] = (((unsigned int)low_memory_table) & PDE_ADDRESS_MASK)| PDE_PRESENT;
     low_memory_table[OFFSET_VIDEO/OFFSET_4K] = OFFSET_VIDEO | PDE_PRESENT;
-    page_directory[1] = OFFSET_4M | PDE_SIZE | PDE_PRESENT;
+    page_directories[KERNEL_PD][1] = OFFSET_4M | PDE_SIZE | PDE_PRESENT;
     asm volatile(
          "mov %0, %%cr3;      \
                               \
@@ -45,7 +47,7 @@ void init_paging(void){
           mov %%eax, %%cr0;   \
           "
     : 
-    : "r" (page_directory), "i" (PAGING_PSE), "i" (PAGING_ENABLE)
+    : "r" (page_directories[KERNEL_PD]), "i" (PAGING_PSE), "i" (PAGING_ENABLE)
     : "ax", "cc","memory" 
     );
 }
