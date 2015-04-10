87c357361344243d6a1d99dec443b97cb32d914a
Dan Frei <frei@outlook.com>
2015-04-10 00:55:18 -0500

    put some magics in the page.h so i can use it elsewhere

--- student-distrib/page.h
+++ student-distrib/page.h
@@ -1,5 +1,8 @@
 #ifndef PAGE_H
 #define PAGE_H
+#define OFFSET_4M      0x400000
+#define OFFSET_4K      0x1000
+
 extern void init_paging(void);
 extern void init_pd(int pd_num);
 extern void set_cr3(int pd_num);
