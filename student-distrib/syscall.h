f12897b7f17b391e44b4cb314cb637b7e55b2402
Rachel Frei <1476684+frei0@users.noreply.github.com>
2015-04-13 16:36:39 -0500

    finish demagic on syscall.S

--- student-distrib/syscall.h
+++ student-distrib/syscall.h
@@ -2,14 +2,11 @@
 #define SYSCALL_H
 #include "x86_desc.h"
 #include "syscall_funcs.h"
+#define SYSCALL_MAX_INDEX 9
+#define EFLAG_INT_ENABLE 0x200
 #ifndef ASM
 #include "lib.h"
 #include "tasking.h"
-
-#define MAX_SYS_INDEX 9
-#define EFLAG_INT_ENABLE 0x200
-#define ONE_BYTE		4
-#define THREE_BYTE	
 
 extern void syscall();
 extern void halt();
