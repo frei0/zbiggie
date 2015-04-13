d3b4f94a475ff9c91c09d3986e9880e4b049047a
natek007 <kaufman.nathaniel@gmail.com>
2015-04-13 16:23:32 -0500

    Half fixed some magic numbers, code not broken

--- student-distrib/syscall.h
+++ student-distrib/syscall.h
@@ -6,6 +6,11 @@
 #include "lib.h"
 #include "tasking.h"
 
+#define MAX_SYS_INDEX 9
+#define EFLAG_INT_ENABLE 0x200
+#define ONE_BYTE		4
+#define THREE_BYTE	
+
 extern void syscall();
 extern void halt();
 
