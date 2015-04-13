db5afbd7d2b50ceb10a631b7bde11786c36151a2
btmoore95 <BenooreM@wirelessprvnat-172-17-12-63.near.illinois.edu>
2015-04-12 22:23:51 -0500

    Fixed Magic Numbers in syscall_funcs.c

--- student-distrib/syscall_funcs.c
+++ student-distrib/syscall_funcs.c
@@ -3,6 +3,13 @@
 #include "x86_desc.h"
 #include "page.h"
 #define MAGIC_NUM 0x464c457f
+#define BUFF_SIZE 128
+#define CHAR_NUM    32
+#define LAST_INDEX	127 
+#define OFFSET 24
+#define ZERO 0 
+#define ONE 1
+#define FAIL -1 
 #define EXE_LOAD_SZ ( 33 * OFFSET_4M - LOAD_ADDR)
 
 /* - - - - - - - - - - - - - - - - - - - - - - 
@@ -13,9 +20,9 @@ void * load_exec_to_mem( const char * fname)
 {
  	FILE f;
 
-	char arg1[128]; 
-	char arg2[128];
-	char arg3[128]; 
+	char arg1[BUFF_SIZE]; 
+	char arg2[BUFF_SIZE];
+	char arg3[BUFF_SIZE]; 
 
 	buffer_parser((char *)&arg1, (char *)&arg2, (char *)&arg3, fname); 
     char * mem = (char *) LOAD_ADDR;
@@ -23,13 +30,8 @@ void * load_exec_to_mem( const char * fname)
    
     kopen(&f, arg1);
 	kread(&f, mem, EXE_LOAD_SZ);
-    
-/*
-	FILE outf;
-	stdout_open(&outf);
-	term_write(&outf, mem, 200); putc('\n');
-*/
-    void * entry = (void *)(*((int*)(mem+24)));
+   
+    void * entry = (void *)(*((int*)(mem+OFFSET)));
     return entry;
 }
 
@@ -38,86 +40,86 @@ int exec_check(const char * fname)
 {
     FILE f;
 
-	char arg1[128]; 
-	char arg2[128];
-	char arg3[128]; 
+	char arg1[BUFF_SIZE]; 
+	char arg2[BUFF_SIZE];
+	char arg3[BUFF_SIZE]; 
 
 	buffer_parser((char *)&arg1, (char *)&arg2, (char *)&arg3, fname); 
     char buf[40];
-	if (kopen(&f, arg1)) { return 0; }
+	if (kopen(&f, arg1)) { return ZERO;}
 	kread(&f, buf, 30);
     if(*(int*)buf != MAGIC_NUM)
     {
-        return 0;
+        return ZERO;
     }
-    return 1;
+    return ONE;
 }
 
 int buffer_parser(char * arg1, char * arg2, char * arg3, const char * s)
 {
     int i, j; 
-    int arg1_first = -1, arg1_last = -1; 
-    int arg2_first = -1, arg2_last = -1; 
-    int arg3_first = -1, arg3_last = -1;
-    int flag1 = 0, flag2 = 0, flag3 = 0; 
-    int end = -1; 
+    int arg1_first = FAIL, arg1_last = FAIL; 
+    int arg2_first = FAIL, arg2_last = FAIL; 
+    int arg3_first = FAIL, arg3_last = FAIL;
+    int flag1 = ZERO, flag2 = ZERO, flag3 = ZERO; 
+    int end = FAIL; 
 
     /* - - - - - - - - - - - - - - - - - - - - - - 
         TEXT Buffer = empty condition 
      - - - - - - - - - - - - - - - - - - - - - - */ 
-    for (i = 0; i < 128; i++)
+    for (i = ZERO; i < BUFF_SIZE; i++)
     {
-        if ( (s[i] == '\n') || (s[i] == 0) ) 
+        if ( (s[i] == '\n') || (s[i] == ZERO) ) 
         {
             end = i; 
             break;
         }
     }
-    if (end == 0)
-        return -1;
+    if (end == ZERO)
+        return FAIL;
 
 
 
     /* - - - - - - - - - - - - - - - - - - - - - - 
         Finding the first term 
      - - - - - - - - - - - - - - - - - - - - - - */ 
-    for (i = 0; i < end; i++)
+    for (i = ZERO; i < end; i++)
     {   
-        if ( (32 < s[i]) && (s[i] < 127) )
+        if ( (CHAR_NUM < s[i]) && (s[i] < LAST_INDEX) )
         {
             arg1_first = i; 
-            flag1 = 1; 
+            flag1 = ONE; 
             break; 
         }
     }
 
-    if (arg1_first == -1)
-        return -1; 
+    if (arg1_first == FAIL)
+        return FAIL; 
 
-    for (i = arg1_first+1; i <= end; i++)
+    for (i = arg1_first+ONE; i <= end; i++)
     {
-        if (!((32 < s[i]) && (s[i] < 127))) 
+        if (!((CHAR_NUM < s[i]) && (s[i] < LAST_INDEX))) 
         {
             arg1_last = i; 
             break;
         }
     }
 
-    if (arg1_last == -1)
-        return -1; 
+    if (arg1_last == FAIL)
+        return FAIL; 
 
-    j = 0; 
+    j = ZERO; 
     for (i = arg1_first; i < arg1_last; i++)
     {
         arg1[j] = s[i]; 
         j++; 
     }
-    arg1[j] = 0; 
+    arg1[j] = ZERO; 
 
     if (flag1){
-        //printf("Term 1: %s\n", arg1);
+			//Do nothing
     }else{
-        arg1[0] = 0;  
+        arg1[ZERO] = ZERO;  
     }
     
 
@@ -127,38 +129,38 @@ int buffer_parser(char * arg1, char * arg2, char * arg3, const char * s)
      - - - - - - - - - - - - - - - - - - - - - - */
     for (i = arg1_last; i < end; i++)
     {   
-        if ( (32 < s[i]) && (s[i] < 127) )
+        if ( (CHAR_NUM < s[i]) && (s[i] < LAST_INDEX) )
         {
             arg2_first = i; 
-            flag2 = 1; 
+            flag2 = ONE; 
             break; 
         }
     }
 
-    for (i = arg2_first+1; i <= end; i++)
+    for (i = arg2_first+ONE; i <= end; i++)
     {
-        if (!((32 < s[i]) && (s[i] < 127))) 
+        if (!((CHAR_NUM < s[i]) && (s[i] < LAST_INDEX))) 
         {
             arg2_last = i; 
             break;
         }
     }
 
-    if (arg2_last == -1)
-        return -1; 
+    if (arg2_last == FAIL)
+        return FAIL; 
 
-    j = 0; 
+    j = ZERO; 
     for (i = arg2_first; i < arg2_last; i++)
     {
         arg2[j] = s[i]; 
         j++; 
     }
-    arg2[j] = 0;
+    arg2[j] = ZERO;
 
     if (flag2 && flag1){
         //printf("Term 2: %s\n", arg2);
     }else{
-        arg2[0] = 0;  
+        arg2[ZERO] = ZERO;  
     }
  
 
@@ -169,45 +171,45 @@ int buffer_parser(char * arg1, char * arg2, char * arg3, const char * s)
      - - - - - - - - - - - - - - - - - - - - - - */
     for (i = arg2_last; i < end; i++)
     {   
-        if ( (32 < s[i]) && (s[i] < 127) )
+        if ( (CHAR_NUM < s[i]) && (s[i] < LAST_INDEX) )
         {
             arg3_first = i; 
-            flag3 = 1; 
+            flag3 = ONE; 
             break; 
         }
     }
 
-    for (i = arg3_first+1; i <= end; i++)
+    for (i = arg3_first+ONE; i <= end; i++)
     {
-        if (!((32 < s[i]) && (s[i] < 127))) 
+        if (!((CHAR_NUM < s[i]) && (s[i] < LAST_INDEX))) 
         {
             arg3_last = i; 
             break;
         }
     }
 
-    if (arg3_last == -1)
-        return -1; 
+    if (arg3_last == FAIL)
+        return FAIL; 
 
-    j = 0; 
+    j = ZERO; 
     for (i = arg3_first; i < arg3_last; i++)
     {
         arg3[j] = s[i]; 
         j++; 
     }
-    arg3[j] = 0;
+    arg3[j] = ZERO;
 
     if (flag3 && flag2 && flag1){
         //printf("Term 3: %s\n", arg3);
     }else{
-        arg3[0] = 0;  
+        arg3[ZERO] = ZERO;  
     }
  
 
     /* - - - - - - - - - - - - - - - - - - - - - - 
-       If All goes smoothly, return 0. 
+       If All goes smoothly, return ZERO. 
      - - - - - - - - - - - - - - - - - - - - - - */
-    return 0;
+    return ZERO;
 }
 
 
