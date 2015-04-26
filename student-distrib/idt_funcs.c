ed2e62c85687d94fde9a210529ee64844e78c090
btmoore95 <BenooreM@wirelessprvnat-172-17-118-214.near.illinois.edu>
2015-04-25 22:38:49 -0500

    Added the three buffers

--- student-distrib/idt_funcs.c
+++ student-distrib/idt_funcs.c
@@ -309,17 +309,17 @@ extern void key_handler()
         if ( (in == F_1) && alt_flag )
 		{
 			switch_video(0);
-			printf("FUCKKKKK TERM 1 HERE WE GO SON");
+			//printf("FUCKKKKK TERM 1 HERE WE GO SON");
 		}
 		else if ( (in == F_2) && alt_flag )
 		{
 			switch_video(1);
-			printf("TERRRRMINAL 2 DAWGS IT GET HOT HERE");	
+			//printf("TERRRRMINAL 2 DAWGS IT GET HOT HERE");	
 		}
 		else if ( (in == F_3) && alt_flag )
 		{
 			switch_video(2);
-			printf("FUCK EYAAAAA. BIGGIE NUMBA 3");
+			//printf("FUCK EYAAAAA. BIGGIE NUMBA 3");
 		}
 		else if ( (scan2ASCII[(int)in] == 'l') && ctrl_flag)
         {
@@ -360,7 +360,7 @@ extern void rtc_handler()
 extern void pit_handler()
 {
 	cli();
-	printf("got pit");
+	//printf("got pit");
 	sti();
 	send_eoi(PIT_LINE);
 }
