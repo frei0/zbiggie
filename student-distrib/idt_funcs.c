3872910da4635c32feb051ec4ce7f92ffe2daecb
natek007 <kaufman.nathaniel@gmail.com>
2015-04-25 19:31:12 -0500

    Have video memory switching working within a process

--- student-distrib/idt_funcs.c
+++ student-distrib/idt_funcs.c
@@ -5,6 +5,7 @@
 #include "i8259.h"
 #include "rtc.h"
 #include "ece391syscall.h"
+#include "page.h"
 #define USER_EXCEPT_CODE 256
 
 char scan2ASCII[256] = 
@@ -307,14 +308,17 @@ extern void key_handler()
 	{
         if ( (in == F_1) && alt_flag )
 		{
+			switch_video(0);
 			printf("FUCKKKKK TERM 1 HERE WE GO SON");
 		}
 		else if ( (in == F_2) && alt_flag )
 		{
+			switch_video(1);
 			printf("TERRRRMINAL 2 DAWGS IT GET HOT HERE");	
 		}
 		else if ( (in == F_3) && alt_flag )
 		{
+			switch_video(2);
 			printf("FUCK EYAAAAA. BIGGIE NUMBA 3");
 		}
 		else if ( (scan2ASCII[(int)in] == 'l') && ctrl_flag)
