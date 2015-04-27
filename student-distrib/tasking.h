564393347885481680c8f01ddacf008430d0c7c3
Bill <bpenda94@gmail.com>
2015-04-26 19:05:27 -0500

    wrote function to get next task pid + stub for save_queue, compiles now

--- student-distrib/tasking.h
+++ student-distrib/tasking.h
@@ -5,6 +5,7 @@
 #include "terminal.h"
 #define MAX_PID 7
 #define MAX_FILES 8
+#define NUM_THREADS 3
 typedef struct pcb{
     uint32_t esp;
     int parent;
@@ -12,11 +13,15 @@ typedef struct pcb{
     FILE f[MAX_FILES];
     char present; //boolean
 } pcb_t;
+int current_thread;
+int threads[NUM_THREADS]; //PID for each running thread 
 
 int setup_new_process();
 void switch_context(int pid);
 FILE * get_file(int fd);
 int get_current_pid();
+int get_next_task_pid();
+void save_queue();
 void init_pcbs();
 void free_current_pcb();
 
