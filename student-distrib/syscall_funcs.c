b68b03c8fbd3dd98481c2030ade7f06877e0eae7
natek007 <kaufman.nathaniel@gmail.com>
2015-05-03 21:52:48 -0500

    Added some comments for syscall_funcs

--- student-distrib/syscall_funcs.c
+++ student-distrib/syscall_funcs.c
@@ -24,9 +24,11 @@
 *   Function: Returns a file descriptor to the file that should be opened 
 */
 int syscall_open(const char * name){
+    //Check validity of pointer and file descriptor
     if(! usr_ptr_ok(name, strlen(name))) return -1;
     int fd = get_new_fd();
     if (fd == -1) return -1;
+    //Open the file and upon sucess return the file descriptor
     int ret = kopen(get_file(fd), name);
     if (ret){
         free_fd(fd);
@@ -52,7 +54,9 @@ int min(int a, int b){ return a<b?a:b;}
 *   Function: Gets arguments from command line 
 */
 int syscall_getargs(char * buf, int nbytes){
+    //Check the validity of the pointer
     if(! usr_ptr_ok(buf, nbytes)) return -1;
+    //Copy the command string to buffer, and if it is not all copied, return -1
     int bytes_copied = (int)strncpy(buf, get_current_pcb()->cmdstring, min(nbytes, BUF_SIZE));
     if(bytes_copied < nbytes)
         return -1;
@@ -66,8 +70,10 @@ int syscall_getargs(char * buf, int nbytes){
 *   Function: Closes a previously opened file
 */
 int syscall_close(int fd){
+    //Get a file and check if it is valid
     FILE * f = get_file(fd);
     if ((int)(f)==-1) return -1;
+    //If it is close it and return based on sucess of free_fd
     kclose(f);
     return free_fd(fd);
 }
@@ -84,25 +90,31 @@ void * load_exec_to_mem( const char * fname)
 
 	char exec[BUF_SIZE]; 
 	char args[BUF_SIZE];
-
+    //Get input fom command line
     parse_input(fname, exec, args, BUF_SIZE);
     char buf[EXE_HEADER_SZ];
 
     dentry_t d;
+    //Read the file and check for type
     read_dentry_by_name(exec, &d);
     if (d.ftype != FTYPE_REGULAR) {return (void*) FAIL;}
 
+    //Open the file and load it into memory
 	if (kopen(&f, exec)) { return (void*)FAIL;}
     char * mem = (char *) LOAD_ADDR;
 	kread(&f, buf, EXE_HEADER_SZ);
 
+    //Check that it is an executable
     if(*(int*)buf != MAGIC_NUM) {
         return (void*)FAIL;
     }
 
+    //Check to see if seting up a new process was sucessful
     if (setup_new_process()){
         return (void*)ONE;
     }
+    //Copy it the file into memory and then create a pointer that is the entry
+    // to the file's location
     memcpy(mem, buf, EXE_HEADER_SZ);
 	kread(&f, mem+EXE_HEADER_SZ, EXE_LOAD_SZ);
    
@@ -128,10 +140,10 @@ void parse_input(const char * in, char * exec_buf, char * args_buf, int size)
 
     if(in == 0)
         return;
-
+    //Ignore leading spaces
     while(in[in_i] == ' ')
         in_i++;
-
+    //Get first word of command into the executable name
     for(out_i = 0; in_i < size && in[in_i] != 0 && in[in_i] != '\n'; in_i++, out_i++)
     {
         if(in[in_i] == ' ')
@@ -139,10 +151,10 @@ void parse_input(const char * in, char * exec_buf, char * args_buf, int size)
         exec_buf[out_i] = in[in_i];
     }
     exec_buf[out_i] = '\0';
-
+    //Ignore spaces
     while(in[in_i] == ' ')
         in_i++;
-
+    //Get all of the arguments and put them into args buffer
     for(out_i = 0; in_i < size && in[in_i] != 0 && in[in_i] != '\n'; in_i++, out_i++)
     {
         args_buf[out_i] = in[in_i];
@@ -159,8 +171,10 @@ void parse_input(const char * in, char * exec_buf, char * args_buf, int size)
 */
 int syscall_vidmap(uint8_t ** vid_ptr)
 {
+    //Check validity of pointer
     if(! usr_ptr_ok(vid_ptr, sizeof(uint8_t *)))
             return -1;
+    //Set video pointer to virtual memory location of video memory
     *(vid_ptr) = (uint8_t *)OFFSET_132M+OFFSET_VIDEO;
     return 0;
 }
@@ -173,6 +187,7 @@ int syscall_vidmap(uint8_t ** vid_ptr)
 *   Function: Checks validity of pointer 
 */
 int usr_ptr_ok(const void * p, uint32_t s){
+    //Check to make sure if pointer is inside user space
     if (((int) p) >= OFFSET_128M && ((int)p) + s <= OFFSET_132M) return 1;
     return 0;
 }
